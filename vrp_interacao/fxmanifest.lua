d1328b34e57d54d5ce9ea7af40c33da9a3f4a674
Passos <66309118+GabrielPPassos@users.noreply.github.com>
2020-07-16 11:58:08 -0300

    Update fxmanifest.lua

--- vrp_interacao/fxmanifest.lua
+++ vrp_interacao/fxmanifest.lua
@@ -1,5 +1,5 @@
 fx_version 'bodacious'
 game 'gta5'
-
-client_scripts { "@vrp/lib/utils.lua", "client.lua", "config/cfg.lua" }
-server_scripts { "@vrp/lib/utils.lua", "server.lua", "config/cfg.lua" }
+-- Criado por Passos#3717
+client_scripts { "@vrp/lib/utils.lua", "client.lua" }
+server_scripts { "@vrp/lib/utils.lua", "server.lua" }
