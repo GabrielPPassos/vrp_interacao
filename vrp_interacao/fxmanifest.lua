eb1ae770a9f03af73511c1e24cc6f5fee25da196
Passos <66309118+GabrielPPassos@users.noreply.github.com>
2020-07-16 11:54:45 -0300

    Add files via upload

---
+++ vrp_interacao/fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'bodacious'
+game 'gta5'
+
+client_scripts { "@vrp/lib/utils.lua", "client.lua", "config/cfg.lua" }
+server_scripts { "@vrp/lib/utils.lua", "server.lua", "config/cfg.lua" }
